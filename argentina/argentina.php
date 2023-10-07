6e7dc2c94e0c0b65ece41c4bc7d69e4eaa3a27d5
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-10-07 13:28:37 -0300

    ex 1

--- argentina/argentina.php
+++ argentina/argentina.php
@@ -1,6 +1,7 @@
 
 <?php 
-require_once 'config.php';
+require_once'config.php';
+require_once'utils.php';
 
 $method = $_SERVER['REQUEST_METHOD'];
 
@@ -8,17 +9,35 @@ $method = $_SERVER['REQUEST_METHOD'];
 
         $body = getBody();
           
-          $name = filter_var($body->name , FILTER_SANITIZE_SPECIAL_CHARS );
-          $contact = filter_var($body->contact , FILTER_SANITIZE_SPECIAL_CHARS );
-          $openingHours = filter_var($body->openingHours , FILTER_SANITIZE_SPECIAL_CHARS );
-          $description = filter_var($body->description , FILTER_SANITIZE_SPECIAL_CHARS );
+          $name = sanitizeString($body->$name);
+          $contact = sanitizeString($body->$contact);
+          $openingHours = sanitizeString($body->$openingHours);
+          $description = sanitizeString($body->$description);
           $latitude = filter_var($body->latitude , FILTER_VALIDATE_FLOAT);
           $longitude = filter_var($body->longitude , FILTER_VALIDATE_FLOAT );
       
 
-    if (!$guiche) {
-        echo json_encode(['error' => 'Não foi enviado o Nº do guiche']);
+    if (!$name || !$contact || !$openingHours || !$description || !$latitude || !$longitude) {
+       responseError('Faltam dados!', 400) ;
+       
     }
-    }
+    
+    $data = [
+        'name' => $name,
+        'contact' => $contact,
+        'opening_Hours' => $openingHours,
+        'description' => $description,
+        'latitude' => $latitude,
+        'longitude' => $longitude
+    ];
 
-?>
+
+    $allData = readFileContent(FILE_CITY);
+    array_push($allData, $data);
+    saveFileContent(FILE_CITY, $allData);
+
+    responseCode($data, 201);
+    
+  
+
+}
--- argentina/config.php
+++ argentina/config.php
@@ -1,10 +1,6 @@
 <?php
 
-define('ARQUIVO_FILA_ATENDIMENTO', 'filaAtendimento.txt');
-define('GUICHE_1', 'guiche1.txt');
-define('GUICHE_2', 'guiche2.txt');
-define('GUICHE_3', 'guiche3.txt');
-
+define('FILE_CITY', 'argentina.txt');
 
 header('Content-Type: application/json');
 header("Access-Control-Allow-Origin: *");
