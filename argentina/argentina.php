03cb88d195bc10dec95177f1261b8040b1631041
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2023-10-09 20:41:43 -0300

    adiciona isset

--- argentina/argentina.php
+++ argentina/argentina.php
@@ -87,18 +87,18 @@ if ($method === 'POST') {
     var_dump($id);
 
     $allData = readFileContent(FILE_CITY);
-
-    foreach($allData as $position => $item) {
-        if($item->id === $id) {
-            $allData[$position]->name = $body->name;
-            $allData[$position]->contact = $body->contact;
-            $allData[$position]->opening_hours = $body->opening_hours;
-            $allData[$position]->description = $body->description;
-            $allData[$position]->latitude = $body->latitude;
-            $allData[$position]->longitude = $body->longitude;
-            
+    foreach ($allData as $position => $item) {
+        if ($item->id === $id) {
+            $allData[$position]->name =  isset($body->name) ? $body->name : $item->name;
+            $allData[$position]->contact =  isset($body->contact) ? $body->contact : $item->contact;
+            $allData[$position]->opening_hours =   isset($body->opening_hours) ? $body->opening_hours : $item->opening_hours;
+            $allData[$position]->description =  isset($body->description) ? $body->description : $item->description;
+            $allData[$position]->latitude =  isset($body->latitude) ? $body->latitude : $item->latitude;
+            $allData[$position]->longitude =  isset($body->longitude) ? $body->longitude : $item->longitude;
         }
     }
-    
+
     saveFileContent(FILE_CITY, $allData);
+
+    response([], 200);
 }
