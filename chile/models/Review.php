d735989a95bf3c06724602efe19f9c1be52b8a61
0Duarte <guiduartesbs@hotmail.com>
2023-10-15 16:56:21 -0300

    exercício metodo GET

--- chile/models/Review.php
+++ chile/models/Review.php
@@ -1,41 +1,110 @@
-<?php 
-require_once 'utils.php';
+<?php
+require_once './utils.php';
 
-class Review{
+// nome da classe
+// os atributos
+
+/*
+enum ReviewsStatus {
+    case 'plano_g': 'GOLD';
+    case 2: 'FINALIZADO';
+    case 3: 'REPROVADO';
+}
+*/
+
+class Review
+{
     private $id, $name, $email, $stars, $date, $status, $place_id;
 
-    public function __construct($place_id)
+    public function __construct($place_id = null)
     {
         $this->id = uniqid();
         $this->place_id = $place_id;
-        $this ->date = (new DateTime())->format('d/m/y h:m');
+        $this->date = (new DateTime())->format('d/m/Y h:m');
+        $this->status = 'PENDENTE';
     }
-    public function save(){
+
+    public function save()
+    {
         $data = [
-            'id' => $this ->getId(),
-            'name' => $this ->getName(),
-            'email' => $this ->getEmail(),
-            'stars' => $this ->getStars(),
-            'status' => $this ->getStatus(),
-            'place_id' => $this ->getId(),
-            'date' => $this -> getDate()
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'email' => $this->getEmail(),
+            'stars' => $this->getStars(),
+            'status' => $this->getStatus(),
+            'date' => $this->getDate(),
+            'place_id' => $this->getPlaceId()
         ];
-        $allData= readFileContent('reviews.txt');
-        array_push($allData, $data);
+
+        $allData = readFileContent('reviews.txt');
+        array_push($allData,  $data);
         saveFileContent('reviews.txt', $allData);
     }
 
-    public function getId() {return $this->id;}
-    public function getName() {return $this->name;}
-    public function setName($name){$this->name = $name;}
-    public function getEmail(){return $this->email;}
-    public function setEmail($email){$this->email = $email;}
-    public function getStars(){return $this->stars;}
-    public function setStars($stars){$this->stars = $stars;}
-    public function getDate(){return $this->date;}
-    public function getStatus(){return $this->status;}
-    public function setStatus($status){$this->status = $status;}
-    public function getPlace_id(){return $this->place_id;}
-    public function setPlace_id($place_id){$this->place_id = $place_id;}
+    public function list()
+    {
+        $allData = readFileContent('reviews.txt');
+
+        $filtered = array_values(array_filter($allData, function ($review) {            
+            return $review->place_id === $this->getPlaceId();
+        }));
+
+        return $filtered;
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getStars()
+    {
+        return $this->stars;
+    }
+
+    public function setStars($stars)
+    {
+        $this->stars = $stars;
+    }
+
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+
+    public function getPlaceId()
+    {
+        return $this->place_id;
+    }
 }
-?>
--- chile/reviews.php
+++ chile/reviews.php
@@ -1,40 +1,67 @@
-<?php 
-require_once "utils.php";
-require_once "config.php";
-require_once "models/Review.php";
+<?php
+require_once 'config.php';
+require_once 'utils.php';
+require_once 'models/Review.php';
+
 
 $method = $_SERVER['REQUEST_METHOD'];
 
-$blacklist = ['polimorfismo', 'herança', 'abstração'];
+/*
+polimorfismo
+herança
+Abstração
+Encapsulamento
+*/
 
-if ($method === "POST"){
+$blacklist = ['polimorfismo',  'herança', 'abstração', 'encapsulamento'];
+
+if ($method === 'POST') {
     $body = getBody();
-    
+
     $place_id = sanitizeInput($body, 'place_id', FILTER_VALIDATE_INT);
     $name = sanitizeInput($body, 'name', FILTER_SANITIZE_SPECIAL_CHARS);
     $email = sanitizeInput($body, 'email', FILTER_VALIDATE_EMAIL);
     $stars = sanitizeInput($body, 'stars', FILTER_VALIDATE_FLOAT);
-    $status = sanitizeInput($body, 'status', FILTER_SANITIZE_SPECIAL_CHARS);
 
-    if(!$place_id) responseError('Id do lugar ausente', 400);
-    if(!$name) responseError('Descricao da avaliacao ausente', 400);
-    if(!$email) responseError('Email ausente', 400);
-    if(!$stars) responseError('Quantidade de estrelas ausente', 400);
-    if(!$status) responseError('Status ausente', 400);
+    if (!$place_id) responseError('Id do lugar ausente', 400);
+    if (!$name) responseError('Descrição da avaliação ausente', 400);
+    if (!$email) responseError('Email inválido', 400);
+    if (!$stars) responseError('Quantidade de estrelas ausente', 400);
 
-    if(strlen($name) > 200) responseError('O texto ultrapassou o limite',400);
+    if (strlen($name) > 200) responseError('O texto ultrapassou o limite', 400);
 
     foreach ($blacklist as $word) {
-        if (str_contains($name, $word)) {
-            $name = str_replace($word, '[EDITADO PELO ADMIN]', $name);
-        };
+        if (str_contains(strtolower($name), $word)) {
+            $name = str_ireplace($word, '😷', $name);
+        }
     }
-    $review = new Review ($place_id);
-    $review -> setName($name);
-    $review -> setEmail($email);
-    $review -> setStars($stars);
-    $review -> setStatus($status);
-    $review -> save();
+
+    $review = new Review($place_id);
+    $review->setName($name);
+    $review->setEmail($email);
+    $review->setStars($stars);
+    $review->save();
 
     response(['message' => 'Cadastro com sucesso'], 201);
-}   
+} else if ($method = 'GET') {
+
+    $place_id = sanitizeInput($_GET,  'id', FILTER_VALIDATE_INT, false);
+
+    if (!$place_id) responseError('ID do lugar está ausente', 400);
+
+    $reviews = new Review($place_id);
+
+    response($reviews->list(), 200);
+} else if ($method = "PUT") {
+    $body = getBody();
+    $id =  sanitizeInput($_GET, 'id', FILTER_VALIDATE_INT, false);
+
+    $status = sanitizeInput($body,  'status', FILTER_SANITIZE_SPECIAL_CHARS);
+
+    if (!$status) {
+        responseError('Status ausente', 400);
+    }
+
+    $review = new Review();
+
+}
