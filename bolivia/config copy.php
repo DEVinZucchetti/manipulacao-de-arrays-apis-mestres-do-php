4ecf2fb42f5e8e0f7b92ee63ca17c6e0d7499370
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-06 12:52:20 +0000

    deletando copia do arquivo criado sem querer

--- bolivia/config copy.php
+++
@@ -1,8 +0,0 @@
-<?php
-define('LOCAIS', 'bolivia.txt');
-
-header("Content-Type: application/json"); // a aplicação retorna json
-header("Access-Control-Allow-Origin: *"); // vai aceitar requisições de todas origens
-header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS"); // habilita métodos
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
-
