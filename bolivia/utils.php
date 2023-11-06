37bc1bfaed6049f193e87d3d380186011b42c414
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-06 12:51:46 +0000

    inserindo as funcoes que serao utilizadas

--- bolivia/utils.php
+++ bolivia/utils.php
@@ -1,30 +1,35 @@
 <?php
 
-function getBody() {
+function getBody()
+{
   return json_decode(file_get_contents("php://input")); // pegar o body no formato de string
 }
 
-function readFileContent($fileName){
-    return json_decode(file_get_contents($fileName));
- }
-
-
-function saveFileContent($fileName, $content) {
-    file_put_contents($fileName, json_encode($content));
+function readFileContent($fileName)
+{
+  return json_decode(file_get_contents($fileName));
 }
 
-function sanitizeString($value){
-  return filter_var($value,FILTER_SANITIZE_SPECIAL_CHARS);
+
+function saveFileContent($fileName, $content)
+{
+  file_put_contents($fileName, json_encode($content));
 }
 
-function responseError($message,$status){
-  return    http_response_code($status);
-    echo json_encode(['error' => $message]); 
-    exit;
+function sanitizeString($value)
+{
+  return filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
 }
 
-function response($response,$status){
- return http_response_code($status);
-    echo json_encode($response);
-    exit;
+function responseError($message, $status)
+{
+  http_response_code($status);
+  echo json_encode(['error' => $message]);
+}
+
+function response($response, $status)
+{
+  return http_response_code($status);
+  echo json_encode($response);
+  exit;
 }
