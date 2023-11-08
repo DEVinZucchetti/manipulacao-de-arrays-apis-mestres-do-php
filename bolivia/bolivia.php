a5285c77f5a82a4f00dd7824b00e54f36fe12887
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-08 02:43:30 +0000

    adicionando a rota PUT

--- bolivia/bolivia.php
+++ bolivia/bolivia.php
@@ -86,5 +86,30 @@ if ($method === 'POST') {
             response($item, 200);
         }
     }
+}else if ($method === 'PUT') {
+    $body = getBody();
+    $id = filter_var($_GET['id'], FILTER_VALIDATE_INT);
+
+    if (!$id) {
+        responseError('ID ausente', 400);
+    }
+
+    $allData = readFileContent(LOCAIS);
+
+
+    foreach ($allData as $position => $item) {
+        if ($item->id === $id) {
+            $allData[$position]->name =  isset($body->name) ? $body->name : $item->name;
+            $allData[$position]->contact =  isset($body->contact) ? $body->contact : $item->contact;
+            $allData[$position]->opening_hours =   isset($body->opening_hours) ? $body->opening_hours : $item->opening_hours;
+            $allData[$position]->description =  isset($body->description) ? $body->description : $item->description;
+            $allData[$position]->latitude =  isset($body->latitude) ? $body->latitude : $item->latitude;
+            $allData[$position]->longitude =  isset($body->longitude) ? $body->longitude : $item->longitude;
+        }
+    }
+
+    saveFileContent(LOCAIS, $allData);
+
+    response([], 200);
 }
 ?>
