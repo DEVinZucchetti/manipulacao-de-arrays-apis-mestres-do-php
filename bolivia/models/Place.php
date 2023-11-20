db4f057f65cd61da3498f6d5503a71821dd4c7c8
fnascimentosilva <fabricionsilva26@gmail.com>
2023-11-20 12:18:43 +0000

    melhorando o codigo

--- bolivia/models/Place.php
+++ bolivia/models/Place.php
@@ -1,7 +1,8 @@
-<?php 
+<?php
 require_once 'config.php';
 
-class Place{
+class Place
+{
 
     public $id;
     private $name;
@@ -11,10 +12,75 @@ class Place{
     private $latitude;
     private $longitude;
 
-    public function __construct($name = null) {
-       $this-> id = uniqid();
-       $this->name = $name;
+    public function __construct($name = null)
+    {
+        $this->id = uniqid();
+        $this->name = $name;
     }
+
+    public function save()
+    {
+        $data = [
+            'id' => $this->getId(),
+            'name' => $this->getName(),
+            'contact' => $this->getContact(),
+            'opening_hours' => $this->getOpeningHours(),
+            'description' => $this->getDescription(),
+            'latitude' => $this->getLatitude(),
+            'longitude' => $this->getLongitude()
+        ];
+        $allData = readFileContent(LOCAIS);
+        array_push($allData,  $data);
+        saveFileContent(LOCAIS, $allData);
+    }
+
+    public function list()
+    {
+        $allData = readFileContent(LOCAIS);
+        return $allData;
+    }
+
+    public function delete($id)
+    {
+        $allData = readFileContent(LOCAIS);
+        $itemsFiltered = array_values(array_filter($allData, function ($item) use ($id) {
+            return $item->id !== $id;
+        }));
+        saveFileContent(LOCAIS, $itemsFiltered);
+    }
+
+    public function update($id, $data)
+    {
+        $allData = readFileContent(LOCAIS);
+
+
+        foreach ($allData as $position => $item) {
+            if ($item->id === $id) {
+                $allData[$position]->name =  isset($data->name) ? $data->name : $item->name;
+                $allData[$position]->contact =  isset($data->contact) ? $data->contact : $item->contact;
+                $allData[$position]->opening_hours =   isset($data->opening_hours) ? $data->opening_hours : $item->opening_hours;
+                $allData[$position]->description =  isset($data->description) ? $data->description : $item->description;
+                $allData[$position]->latitude =  isset($data->latitude) ? $data->latitude : $item->latitude;
+                $allData[$position]->longitude =  isset($data->longitude) ? $data->longitude : $item->longitude;
+            }
+
+            saveFileContent(LOCAIS, $allData);
+        }
+    }
+
+    public function listOne($id)
+    {
+
+        $allData = readFileContent(LOCAIS);
+
+        foreach ($allData as $item) {
+            if ($item->id === $id) {
+               return $item;
+            }
+        }
+    }
+
+
 
     public function getId()
     {
@@ -29,7 +95,6 @@ class Place{
     public function setName($name)
     {
         $this->name = $name;
-
     }
 
     public function getContact()
@@ -40,7 +105,6 @@ class Place{
     public function setContact($contact)
     {
         $this->contact = $contact;
-
     }
 
     public function getOpeningHours()
@@ -51,7 +115,6 @@ class Place{
     public function setOpeningHours($opening_hours)
     {
         $this->opening_hours = $opening_hours;
-
     }
 
     public function getDescription()
@@ -62,7 +125,6 @@ class Place{
     public function setDescription($description)
     {
         $this->description = $description;
-
     }
 
     public function getLatitude()
@@ -72,7 +134,6 @@ class Place{
     public function setLatitude($latitude)
     {
         $this->latitude = $latitude;
-
     }
 
     public function getLongitude()
@@ -83,12 +144,5 @@ class Place{
     public function setLongitude($longitude)
     {
         $this->longitude = $longitude;
-
     }
-
-
 }
-
-
-
-?>
